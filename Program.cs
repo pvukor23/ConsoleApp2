62d3a2512c3cf468d9bf111677225543b275ed9e
Pero444 <malesjure9@gmail.com>
2025-03-17 20:02:09 +0100

    .

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleAppp2
+{
+    class Program
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
